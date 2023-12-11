85c7ef752833d39831530d556b53a3595b3167d9
rpantojab@gmail.com <rpantojab@gmail.com>
2023-12-11 21:14:15 +0000

    Commit Employees

--- employees/webapp/controller/View1.controller.js
+++ employees/webapp/controller/View1.controller.js
@@ -7,11 +7,50 @@ sap.ui.define([
     function (Controller) {
         "use strict";
 
-        return Controller.extend("logaligroup.employees.controller.View1", {
-            onInit: function () {
 
-            },
-            onChangeEmployee: function () {
+        function onInit() {
+            var oComponet  = this.getOwnerComponent().getModel("i18n");
+//       const resourceBunble = this.getView().getModel("i18n").getResourceBundle();
+//       console.log(resourceBunble);
+            //var oModel = new JSONModel();
+            var oModel = new sap.ui.model.json.JSONModel();
+            var oView =  this.getView();
+//            var oMi18n = oView.getModel("i18n");
+//            console.log(oMi18n);
+//            //var i18nBundle = oMi18n.getResourceBundle();
+//            //var oBundle = this.getView().getModel("i18n").getResourceBundle();
+
+            var i18nBundle = oComponet.getResourceBundle();
+           
+            // var oJSON = { 
+            //     employeeId: "12345",
+            //     countryKey: "UK",
+            //     listCountry: [
+            //         {
+            //           key: "US",
+            //           text: i18nBundle.getText("countryUS")
+            //         },
+            //         {
+            //             key: "UK",
+            //             text: i18nBundle.getText("countryUK")
+            //           },
+            //           {
+            //             key: "ES",
+            //             text: i18nBundle.getText("countryES")
+            //           },
+            //           {
+            //             key: "CL",
+            //             text: "Chile"
+            //           }
+            // ]};
+            //oModel.setData(oJSON);
+            oModel.loadData("../localService/mockdata/Employees.json");
+            oView.setModel(oModel);
+        }
+
+        var Main = Controller.extend("logaligroup.employees.controller.View1", {});
+    
+        Main.prototype.onChangeEmployee = function () {
                 var inputLabel = this.getView().byId("idInputEmployee");
                 var sEmployes = inputLabel.getValue();
                 if (sEmployes.length === 6) {
@@ -23,6 +62,8 @@ sap.ui.define([
                     this.getView().byId("idLabelCountry").setVisible(false);
                     this.getView().byId("idSelCountry").setVisible(false);
                 }
-            }
-        });
+            };
+       Main.prototype.onInit = onInit;
+       return Main;
+        
     });
